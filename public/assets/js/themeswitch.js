08543f3a151f2dc948397222005713c53f1958ef
mormostrix <mormotheauthor@proton.me>
2023-11-18 14:23:21 -0800

    initial

---
+++ public/assets/js/themeswitch.js
@@ -0,0 +1,18 @@
+function themeSwitch(style) {
+  if (style == null)
+      style = localStorage.getItem("theme");
+      if (style == null)
+          style = "base";
+  document.querySelector("link[type='text/css']").setAttribute("href", "/public/assets/css/themes/" + style + ".css");
+  localStorage.setItem("theme", style);
+}
+
+window.onload = function () {
+  let themeswitcher = document.getElementById("themeswitcher");
+
+  themeswitcher.addEventListener("change", (event) => {
+    themeSwitch(event.target.value);
+  });
+};
+
+themeSwitch();
